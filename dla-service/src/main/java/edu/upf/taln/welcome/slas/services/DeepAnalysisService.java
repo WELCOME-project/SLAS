1ba1b2ed01e2e492dff25d220813d06254511645
rcarlini <roberto.carlini@upf.edu>
2020-07-14 15:14:01 +0200

    Added turn0 and turn1 examples to swagger.

--- dla-service/src/main/java/edu/upf/taln/welcome/slas/services/DeepAnalysisService.java
+++ dla-service/src/main/java/edu/upf/taln/welcome/slas/services/DeepAnalysisService.java
@@ -18,7 +18,9 @@ import org.apache.commons.logging.LogFactory;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 import edu.upf.taln.welcome.slas.commons.exceptions.WelcomeException;
@@ -43,6 +45,42 @@ import edu.upf.taln.welcome.slas.utils.SampleResponses;
 @Produces(MediaType.APPLICATION_JSON)
 public class DeepAnalysisService {
 
+	private static final String SAMPLE_INPUT_TURN0 = "{\n" + 
+			"  \"metadata\": {},\n" + 
+			"  \"data\": {\n" + 
+			"    \"conll\": \"# sent_id = 1\\n" + 
+			"# text = Hello, can you hear me?\\n" + 
+			"1\\tHello\\thello\\tINTJ\\tUH\\t_\\t5\\tdiscourse\\t_\\tSpaceAfter=No\\n" + 
+			"2\\t,\\t,\\tPUNCT\\t,\\t_\\t5\\tpunct\\t_\\t_\\n" + 
+			"3\\tcan\\tcan\\tAUX\\tMD\\tVerbForm=Fin\\t5\\taux\\t_\\t_\\n" + 
+			"4\\tyou\\tyou\\tPRON\\tPRP\\tCase=Nom|Person=2|PronType=Prs\\t5\\tnsubj\\t_\\t_\\n" + 
+			"5\\thear\\thear\\tVERB\\tVB\\tVerbForm=Inf\\t0\\troot\\t_\\t_\\n" + 
+			"6\\tme\\tI\\tPRON\\tPRP\\tCase=Acc|Number=Sing|Person=1|PronType=Prs\\t5\\tobj\\t_\\tSpaceAfter=No\\n" + 
+			"7\\t?\\t?\\tPUNCT\\t.\\t_\\t5\\tpunct\\t_\\tSpacesAfter=\\\\n\\n\"" + 
+			"  } \n" + 
+			"}";
+
+
+	private static final String SAMPLE_INPUT_TURN1 = "{\n" + 
+			"  \"metadata\": {},\n" + 
+			"  \"data\": {\n" + 
+			"    \"conll\": \"# sent_id = 2\\n" + 
+			"# text = Yes, I would like to apply for the First Reception Service.\\n" + 
+			"1\\tYes\\tyes\\tINTJ\\tUH\\t_\\t5\\tdiscourse\\t_\\tSpaceAfter=No\\n" + 
+			"2\\t,\\t,\\tPUNCT\\t,\\t_\\t5\\tpunct\\t_\\t_\\n" + 
+			"3\\tI\\tI\\tPRON\\tPRP\\tCase=Nom|Number=Sing|Person=1|PronType=Prs\\t5\\tnsubj\\t_\\t_\\n" + 
+			"4\\twould\\twould\\tAUX\\tMD\\tVerbForm=Fin\\t5\\taux\\t_\\t_\\n" + 
+			"5\\tlike\\tlike\\tVERB\\tVB\\tVerbForm=Inf\\t0\\troot\\t_\\t_\\n" + 
+			"6\\tto\\tto\\tPART\\tTO\\t_\\t7\\tmark\\t_\\t_\\n" + 
+			"7\\tapply\\tapply\\tVERB\\tVB\\tVerbForm=Inf\\t5\\txcomp\\t_\\t_\\n" + 
+			"8\\tfor\\tfor\\tADP\\tIN\\t_\\t12\\tcase\\t_\\t_\\n" + 
+			"9\\tthe\\tthe\\tDET\\tDT\\tDefinite=Def|PronType=Art\\t12\\tdet\\t_\\t_\\n" + 
+			"10\\tFirst\\tfirst\\tADJ\\tJJ\\tDegree=Pos|NumType=Ord\\t12\\tamod\\t_\\t_\\n" + 
+			"11\\tReception\\treception\\tNOUN\\tNN\\tNumber=Sing\\t12\\tcompound\\t_\\t_\\n" + 
+			"12\\tService\\tservice\\tNOUN\\tNN\\tNumber=Sing\\t7\\tobl\\t_\\tSpaceAfter=No\\n" + 
+			"13\\t.\\t.\\tPUNCT\\t.\\t_\\t5\\tpunct\\t_\\tSpacesAfter=\\\\n\\n\"" + 
+			"  } \n" + 
+			"}";
 	/**
 	 * Logger for this class and subclasses.
 	 */
@@ -88,6 +126,17 @@ public class DeepAnalysisService {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Operation(summary = "Performs a deep syntactic analysis of the input data.",
 		description = "Returns the result of the deep syntatic analysis, it is, a predicate-argument structure.",
+		requestBody = @RequestBody(
+						content = @Content(mediaType = "application/json",
+										schema = @Schema(implementation = DeepAnalysisInput.class),
+										examples = {
+											@ExampleObject(name = "Turn 0",
+													value = SAMPLE_INPUT_TURN0),
+											@ExampleObject(name = "Turn 1",
+													value = SAMPLE_INPUT_TURN1)
+										}
+						)
+					),
 		responses = {
 		        @ApiResponse(description = "The deep analysis result.",
 		        			content = @Content(schema = @Schema(implementation = DeepAnalysisOutput.class)
@@ -97,6 +146,7 @@ public class DeepAnalysisService {
 			@Parameter(description = "Container for analysis input data.", required = true) DeepAnalysisInput input) throws WelcomeException {
 
         String conll = input.getData().getConll();
+        System.out.println(conll);
         
         int turn = 1;
         if (conll.contains("Sebastià")) {
