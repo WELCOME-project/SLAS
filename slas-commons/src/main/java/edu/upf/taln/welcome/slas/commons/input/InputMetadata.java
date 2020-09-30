3bf58eea727633a81c2da42fad38a6e85c4a9f90
rcarlini-upf <roberto.carlini@upf.edu>
2020-09-30 17:43:17 +0200

    Adapted mock version to CUNI input

--- slas-commons/src/main/java/edu/upf/taln/welcome/slas/commons/input/InputMetadata.java
+++ slas-commons/src/main/java/edu/upf/taln/welcome/slas/commons/input/InputMetadata.java
@@ -5,5 +5,15 @@ package edu.upf.taln.welcome.slas.commons.input;
  * @author rcarlini
  */
 public class InputMetadata {
+
+    String language;
+    
+    public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
     
 }
