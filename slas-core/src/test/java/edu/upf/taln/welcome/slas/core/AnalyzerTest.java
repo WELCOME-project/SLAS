c7bce11e83e6ae050a4f964a010e8908bd3d82f5
rcarlini-upf <roberto.carlini@upf.edu>
2020-10-22 11:50:27 +0200

    Added sample test

---
+++ slas-core/src/test/java/edu/upf/taln/welcome/slas/core/AnalyzerTest.java
@@ -0,0 +1,48 @@
+package edu.upf.taln.welcome.slas.core;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import org.apache.commons.io.FileUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import edu.upf.taln.welcome.slas.commons.exceptions.WelcomeException;
+import edu.upf.taln.welcome.slas.commons.factories.OutputFactory.OutputLevel;
+import edu.upf.taln.welcome.slas.commons.input.DeepAnalysisInput;
+import edu.upf.taln.welcome.slas.commons.output.IAnalysisOutput;
+import edu.upf.taln.welcome.slas.core.factories.JCasWelcomeFactory.InputType;
+import edu.upf.taln.welcome.slas.core.utils.WelcomeUIMAUtils.AnalysisType;
+
+/**
+ *
+ * @author rcarlini
+ */
+public class AnalyzerTest {
+    
+    /**
+     * Test of analyze method, of class Analyzer.
+     * @throws edu.upf.taln.welcome.slas.commons.exceptions.WelcomeException
+     * @throws java.io.IOException
+     */
+    @Test
+    public void testAnalyze() throws WelcomeException, IOException {
+
+        Analyzer analyzer = Analyzer.getInstance();
+        ObjectMapper mapper = new ObjectMapper();
+
+        String inputPath = "src/test/resources/sample_input.json";
+        DeepAnalysisInput input = mapper.readValue(new File(inputPath), DeepAnalysisInput.class);
+
+        ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
+        
+
+        IAnalysisOutput output = analyzer.analyze(InputType.text, AnalysisType.BASIC, input.getData().getConll(), OutputLevel.welcome);
+
+        String result = writer.writeValueAsString(output);
+        System.out.println(result);
+    }    
+}
