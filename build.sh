bc57fe3f72a523f60368b7f3042613d1e408b603
Jens Grivolla <jens.grivolla@upf.edu>
2021-02-11 18:43:30 +0100

    update stack

--- build.sh
+++ build.sh
@@ -38,6 +38,6 @@ done
 mvn -U clean install package -DskipTests
 export TAG=$tag
 
-#docker build -t registry.gitlab.com/talnupf/welcome/slas/dla:${TAG} . && docker push registry.gitlab.com/talnupf/welcome/slas/dla:${TAG}
-#docker build -t steps.everis.com:10110/upf/dla:${TAG} . && docker push steps.everis.com:10110/upf/dla:${TAG}
-docker build -t maven-taln.upf.edu/welcome/dla:${TAG} . && docker push maven-taln.upf.edu/welcome/dla:${TAG}
+docker build -t registry.gitlab.com/talnupf/welcome/slas/dla:${TAG} . && docker push registry.gitlab.com/talnupf/welcome/slas/dla:${TAG}
+#docker build -t maven-taln.upf.edu/welcome/dla:${TAG} . && docker push maven-taln.upf.edu/welcome/dla:${TAG}
+#docker tag registry.gitlab.com/talnupf/welcome/slas/dla:${TAG} nexus-dockers.everis.com:10110/upf/dla:${TAG} && docker push nexus-dockers.everis.com:10110/upf/dla:${TAG}
